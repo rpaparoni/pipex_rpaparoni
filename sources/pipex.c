35bc5538453e9eca71c62ad56813417409ff2df6
Ricardo <paparoniricardo@gmail.com>
2025-02-27 18:46:26 +0100

    corregir tipo int

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,25 +6,25 @@
 /*   By: rpaparon <rpaparon@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 16:10:34 by rpaparon          #+#    #+#             */
-/*   Updated: 2025/02/27 18:41:47 by rpaparon         ###   ########.fr       */
+/*   Updated: 2025/02/27 18:46:08 by rpaparon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/pipex.h"
 
-void	open_files(char *infile, char *outfile, int *in_fd, int *out_fd)
+void	open_files(char *infile, char *outfile, int in_fd, int out_fd)
 {
-	*in_fd = open(infile, O_RDONLY);
-	if(*in_fd < 0)
+	in_fd = open(infile, O_RDONLY);
+	if(in_fd < 0)
 	{
 		perror("Error opening infile");
 		exit(EXIT_FAILURE);
 	}
-	*out_fd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if(*out_fd < 0)
+	out_fd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if(out_fd < 0)
 	{
 		perror("Error opening outfile");
-		close(*in_fd);
+		close(in_fd);
 		exit(EXIT_FAILURE);
 	}
 }
